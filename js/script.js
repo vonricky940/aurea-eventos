4ed160a2a1a813e613342d1f541eebcea6eb0cb9
vonricky940 <vonricky940@gmail.com>
2025-06-21 00:54:35 +0100

    Added About Us section and footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
+const scrollUpBtn = document.getElementById('scrollUpBtn');
+window.onscroll = function () {
+    scrollUpBtn.style.display = window.scrollY > 300 ? 'block' : 'none';
+};
+scrollUpBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
 function changeLanguage(lang) {
-  alert(`Mudar idioma para: ${lang} (funcionalidade em desenvolvimento)`);
+    alert(`Mudar idioma para: ${lang} (funcionalidade em desenvolvimento)`);
 }
+
+const form = document.getElementById("contactForm");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6Ldu5WcrAAAAAGD6FpjV029uN38EviyFVu9vlBrs', { action: 'submit' }).then(function (token) {
+            document.getElementById('g-recaptcha-response').value = token;
+
+            fetch(form.action, {
+                method: "POST",
+                body: new FormData(form),
+            }).then(() => {
+                var modal = new bootstrap.Modal(document.getElementById("successModal"));
+                modal.show();
+                form.reset();
+            }).catch(() => {
+                alert("Erro ao enviar. Por favor tente novamente mais tarde.");
+            });
+        });
+    });
+});
